0ef4bff8281cefd12b48e4629e9ef732511d9beb
dstarrtrey <dstarrtrey@gmail.com>
2019-06-30 23:51:19 -0700

    Fixes portfolio bug and gives meaningful errors if email fails

--- src/email-req.js
+++ src/email-req.js
@@ -1,12 +1,19 @@
 /* eslint-disable */
 
-export default (url, data) => fetch(url, {
-  method: 'POST', // or 'PUT'
-  body: JSON.stringify(data), // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/json'
-  }
-}).then(data => data.json());
+export default async (url, data) => {
+    return await fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json'
+    }
+    })
+      .then(data => {
+        if (!data) throw new Error (false);
+        return data.json()
+      })
+      .catch(err => err);
+}
 
 export function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
